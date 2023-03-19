447d2f99411fceee494691b49574df7d68bee309
motaz <motaz.dn.ali@gmail.com>
2023-03-19 15:19:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+const saturdayFun = (activity = "roller-skate") =>
+  `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity = "go to the office") =>
+  `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (wrapper = "*") => {
+  return (career = "special") => {
+    return `You are ${wrapper}${career}${wrapper}!`;
+  };
+};
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
